eda842448c867f193b0bb60ee59e55aea0e21d1d
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-07 23:11:51 -0700

    Add files via upload

---
+++ Power of Three.py
@@ -0,0 +1,9 @@
+# 326. Power of Three
+
+# Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+# An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 == 3**19 % n
