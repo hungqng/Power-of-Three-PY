085125976f6a979eaf0b58b9ba04c99cd59f9ead
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-07 23:15:25 -0700

    Add files via upload

--- Power of Three.py
+++ Power of Three.py
@@ -7,3 +7,12 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         return n > 0 == 3**19 % n
+
+        # Solution 2
+        # if n <= 0:
+        # 	return False
+
+        # while n % 3 == 0:        	
+        # 	n = n / 3
+
+        # return n == 1
